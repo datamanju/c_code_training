4cab6dd1d044d7f12221cf016ca0623a3e3df78b
AL <alanluo@outlook.com>
2019-12-09 23:46:26 -0500

    Add files via upload

--- ex1.cc
+++ ex1.cc
@@ -5,5 +5,9 @@ int main(int argc, char *argv[])
     int distance = 100;
     //this is also a comment
     printf("You are %d miles away.\n", distance);
+    printf("National lets you choose any car in the aisle & go! Go Like a Pro. \
+    Emerald Priority Service. Benefits for Business. Specials in Your Inbox. Bypass the Counter Today. \
+    Join Emerald Club Now. Choose Any Car & Go. Leave the Lines Behind. National Car Rental. \n");
+    printf("This is the end of the message.\n");
     return 0;
 }
